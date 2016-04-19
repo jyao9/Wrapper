2682227b00a9e263bd2713ef98b9de4c99e63d5c
Jessica Yao <jessicayao3981@gmail.com>
2016-04-19 11:01:23 -0400

    Refactor create li

--- list_maker.js
+++ list_maker.js
@@ -1,44 +1,8 @@
 var $div = $l("ul");
 
-Object.keys(localStorage).forEach( function (key) {
-  if (key.slice(0, 3) === "li-") {
-    var title = localStorage[key];
-    var $li = $l(document.createElement("li"));
-    $li.html(title);
-    $div.append($li);
-
-    $l("li").off("click");
-    $l("li").off("dblclick");
-
-    $l("li").on("click", function (e) {
-      e.preventDefault();
-      var $li = $l(this);
-      if ($li.attr("class") === " done") {
-        $li.removeClass("done");
-      } else if (!$li.attr("class")) {
-        $li.addClass("done");
-      }
-
-    });
-
-    $l("li").on("dblclick", function (e) {
-      e.preventDefault();
-      var $li = $l(this);
-      if ($li.attr("class") === " done") {
-        localStorage.removeItem(key);
-        $li.remove();
-      }
-    });
-  }
-});
-
-$l(".category").on("submit", function (e) {
-  e.preventDefault();
-  var title = document.getElementById('category-title').value;
+var createLi = function (title, key) {
   var $li = $l(document.createElement("li"));
   $li.html(title);
-  var key = "li-" + title;
-  localStorage.setItem(key, title);
   $div.append($li);
 
   $l("li").off("click");
@@ -59,10 +23,29 @@ $l(".category").on("submit", function (e) {
     e.preventDefault();
     var $li = $l(this);
     if ($li.attr("class") === " done") {
-      localStorage.removeItem($li);
+      localStorage.removeItem(key);
       $li.remove();
     }
   });
+};
+
+Object.keys(localStorage).forEach( function (key) {
+  if (key.slice(0, 3) === "li-") {
+    var title = localStorage[key];
+    createLi(title, key);
+  }
+});
+
+$l(".category").off("submit");
+$l(".category").on("submit", function (e) {
+  e.preventDefault();
+  var title = document.getElementById('category-title').value;
+  var key = "li-" + title;
+  localStorage.setItem(key, title);
+
+  var $li = $l(document.createElement("li"));
+  $li.html(title);
+  $div.append($li);
 
   document.getElementById('category-title').value = "";
 });
